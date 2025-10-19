626b457602a41f4f0118c93e01a1ae4259c6fa11
apollo <apollo0x1@proton.me>
2025-10-19 18:44:58 -0500

    Delete src directory

--- src/utils/network.rs
+++
@@ -1,6 +0,0 @@
-pub fn check_privileges() {
-    if unsafe { libc::geteuid() } != 0 {
-        eprintln!("Warning: Raw socket operations require root privileges");
-        eprintln!("Some stealth features may be limited without root access");
-    }
-}
